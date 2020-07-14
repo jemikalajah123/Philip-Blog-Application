1086a6bb5b54dd6a55ead48922778d62344d4e21
jemikalajah123 <philip.jemikalajah@konga.local>
2020-07-14 23:42:10 +0100

    improved image display

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,8 +4,8 @@
     <h1><?php echo $title; ?></h1>
     <p> Hi, I am Philip, a Software Engineer that loves to solve problems.</p>
     <p>This is my first laravel application and i am super excited. It has a blogging feature, please do well to register
-        <a class="btn btn-success btn-sm" href="/register" role="button">Register</a> and Let's blog about our life experiences in 
-        related to education, work, health and whatever you feel like sharing.</p>
+        <a class="btn btn-success btn-sm" href="/register" role="button">Register</a> and Let's blog about our life experiences as
+        it relates to education, work, health and whatever you feel like sharing.</p>
     
     <img style="width: 100%" src="/storage/cover_images/tech.jpg">
 @endsection
