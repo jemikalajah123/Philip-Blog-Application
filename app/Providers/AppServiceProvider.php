dadd1760b82ddee8a7521f5e5d059b7cf66bdcea
jemikalajah123 <philip.jemikalajah@konga.local>
2020-08-29 16:40:56 +0100

    verification url proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if($this->app->environment('production'))
+        {
+            \URL::forceScheme('https');
+        }
     }
 }
